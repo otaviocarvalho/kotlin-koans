7b79c629f3594b52b8f7da40e104b1402622b4d0
Otávio Carvalho <otavio.carvalho@n26.com>
2019-04-28 16:42:11 +0200

    Add solution for collections exercise 23

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,17 +2,27 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return this.customers.filter {
+        it.orders.filter { it.products.contains(product) }.isNotEmpty()
+    }.toSet()
+//    todoCollectionTask()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orders
+            .filter { it.isDelivered }
+            .flatMap { it.products }
+            .maxBy { it.price }
+//    todoCollectionTask()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
+    return this.customers.flatMap { it.orders.flatMap { it.products } }
+            .filter { it.name == product.name }
+            .size
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+//    todoCollectionTask()
 }
